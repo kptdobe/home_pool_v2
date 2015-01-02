de219d73ae3d954d3ae327fce7f013d7b56b0b3d
Alexandre Capt <acapt@adobe.com>
2015-01-02 16:32:19 +0100

    Changed setup

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var app = express();
 
 // parse application/json
 app.use(bodyParser.json());
-app.use("/",express.static(__dirname + '/resources'));
+app.use("/", express.static(__dirname + '/resources'));
 
 var server = app.listen(process.env.PORT || 8080);
 console.log("[START] Initialized server on port " + server.address().port);
@@ -19,7 +19,7 @@ var p = masterConfig.gpio.proxy.port;
 console.log('[START] GPIO API proxied to ' + h + ':' + p + '/api');
 
 //route /api to gpio proxy
-app.all('/api/*', function(req, res){
+app.all('/api/*', function (req, res) {
     var options = {
         hostname: h,
         port: p,
@@ -27,27 +27,27 @@ app.all('/api/*', function(req, res){
         method: req.method
     };
 
-    var creq = http.request(options, function(cres) {
+    var creq = http.request(options, function (cres) {
 
         // set encoding
         cres.setEncoding('utf8');
 
         // wait for data
-        cres.on('data', function(chunk){
+        cres.on('data', function (chunk) {
             //transform plain/text response to JSON
             res.json({
                 value: chunk
             });
         });
 
-        cres.on('close', function(){
+        cres.on('close', function () {
             res.end();
         });
 
-        cres.on('end', function(){
+        cres.on('end', function () {
             res.end();
         });
-    }).on('error', function(e) {
+    }).on('error', function (e) {
         // we got an error, return 500 error to client and log error
         res.writeHead(500);
         res.end();
@@ -61,50 +61,50 @@ var storage = require('./server/persistence.js');
 //route /data to storage
 var rdata = express.Router();
 rdata.route('/data')
-    .post(function(req, res) {
+    .post(function (req, res) {
         storage.write("data", req.body.id, req.body);
         res.json(storage.read("data", req.body.id));
     })
 
-    .get(function(req, res) {
+    .get(function (req, res) {
         res.json(storage.read("data"));
     });
 rdata.route('/data/:id')
-    .get(function(req, res) {
+    .get(function (req, res) {
         res.json(storage.read("data", req.params.id));
     })
-    .put(function(req, res) {
+    .put(function (req, res) {
         storage.write("data", req.params.id, req.body);
         res.json(storage.read("data", req.params.id));
     })
-    .delete(function(req, res) {
+    .delete(function (req, res) {
         storage.removeItem("data", req.params.id);
-        res.json({ message: 'Successfully deleted' });
+        res.json({message: 'Successfully deleted'});
     });
 app.use('/persist', rdata);
 
 //route /tasks to scheduler
 var rtasks = express.Router();
 rtasks.route('/tasks')
-    .post(function(req, res) {
+    .post(function (req, res) {
         storage.write("scheduler", req.body.id, req.body);
         res.json(storage.read("scheduler", req.body.id));
     })
 
-    .get(function(req, res) {
+    .get(function (req, res) {
         res.json(storage.read("scheduler"));
     });
 rtasks.route('/tasks/:id')
-    .get(function(req, res) {
+    .get(function (req, res) {
         res.json(storage.read("scheduler", req.params.id));
     })
-    .put(function(req, res) {
+    .put(function (req, res) {
         storage.write("scheduler", req.params.id, req.body);
         res.json(storage.read("scheduler", req.params.id));
     })
-    .delete(function(req, res) {
+    .delete(function (req, res) {
         storage.removeItem("scheduler", req.params.id);
-        res.json({ message: 'Successfully deleted' });
+        res.json({message: 'Successfully deleted'});
     });
 app.use('/scheduler', rtasks);
 
@@ -119,20 +119,22 @@ app.use('/scheduler', rtasks);
 var rLog = express.Router();
 app.use('/log', rLog);
 
-var logger = require('./server/loggers/sensors')({
-    router: rLog,
-    server: server,
-    db: './home.db'
-});
+if (masterConfig.loggers) {
+    var logger = require('./server/loggers/sensors')({
+        router: rLog,
+        server: server,
+        db: masterConfig.loggers.db
+    });
 
-if( masterConfig.loggers ) {
-    for (var i = 0; i < masterConfig.loggers.length; i++) {
-        var l = masterConfig.loggers[i];
-        console.log('[START] Logging ' + l.url + ' each ' + l.interval + ' ms.');
-        setInterval(function () {
-            logger.log({
-                url: l.url
-            });
-        }, l.interval);
+    if (masterConfig.loggers.crons) {
+        for (var i = 0; i < masterConfig.loggers.crons.length; i++) {
+            var l = masterConfig.loggers.crons[i];
+            console.log('[START] Logging ' + l.url + ' each ' + l.interval + ' ms.');
+            setInterval(function () {
+                logger.log({
+                    url: l.url
+                });
+            }, l.interval);
+        }
     }
 }
